63eb8f5ae9a7f4809cb07d35de83f599d53d469c
PH-WebDeveloper <pedrobatutu@gmail.com>
2020-04-06 03:40:31 -0300

    Foram adicionados os arquivos

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Arquivos SA
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JS
+
+import './js/core/includes'
+import './js/plugins/cityButtons'
